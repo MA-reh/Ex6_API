bfc3bcc0c2bec42abe804fc89661edc233d56b27
Mohamed_Ayman(MÀ) <Mohamedayman152025@gmail.com>
2025-10-02 19:32:35 +0300

    Add files via upload

--- js/data.of.API.js
+++ js/data.of.API.js
@@ -4,39 +4,124 @@ var categorySource;
 var categoryDomains;
 var categoryCountry;
 
-fetch("https://newsapi.org/v2/everything?q=tesla&from=2025-08-29&sortBy=publishedAt&apiKey=0a41a4f5b2174e4ea14efdebefdcbb6e").then((response) => response.json()).then(function (data) {
+fetch(
+  "https://newsapi.org/v2/everything?q=tesla&from=2025-09-02&sortBy=publishedAt&apiKey=49878c3f53a943c28b30ba4476d1d610"
+)
+  .then((response) => response.json())
+  .then(function (data) {
     console.log(data);
     categoryTesla = data.articles;
-    
-    showDataOfAPI(categoryTesla);
-}).catch((Error) => {
-    console.log("Error", Error);
-})
+    ArrayOfData = categoryTesla
+    NumberAllObjInArray = data.articles.length;
+  })
+  .then(() => {
+    (function () {
+      rowNews.innerHTML += ``;
+      updateIndicatorsContent();
+      categoryTesla.forEach(function (data, index) {
+        itemIndex = index + 1;
+        if (itemIndex <= numOfEnd && itemIndex >= numOfStart) {
+          rowNews.innerHTML += `
+                <div class="spacial-card mb-4 col-md-6 col-xl-4">
+                    <div class="item" data-id="${itemIndex}">
+                        <div class="header"
+                            style="background-image: url('${data.urlToImage}');">
+                        </div>
+                        <div class="body text-start">
+                            <h6 class="NameUser">${data.source.id ?? data.source.name ?? "unknown"}</h6>
+                            <h5 class="titleOfNew">
+                                ${data.title?.slice(0, 20)}...
+                            </h5>
+                            <p class="contentNew">
+                                ${data.description === "" ? "Nothing" : data.description?.slice(0, 50)}
+                            </p>
+                            <span class="d-block mb-3 mainColor">
+                                ${data.publishedAt} 
+                            </span>
+                            <a class="" href="${data.url}">See More</a>
+                        </div>
+                    </div>
+                </div>
+                `;
+        } else {
+          return 0;
+        }
+      });
+    })();
+  })
+  .then(() => {
+    searchInput.addEventListener("keyup", () => {
+      inputValue = searchInput.value.toLowerCase();
 
-fetch("https://newsapi.org/v2/everything?q=apple&from=2025-09-28&to=2025-09-28&sortBy=popularity&apiKey=0a41a4f5b2174e4ea14efdebefdcbb6e").then((response) => response.json()).then(function (data) {
+      let filteredNews = ArrayOfData.filter(function (objData) {
+        
+        let findData = objData.author?.includes(inputValue) ||
+        objData.title?.includes(inputValue) ||
+        objData.description?.includes(inputValue) ||
+        objData.publishedAt?.includes(inputValue) ||
+        objData.source.name?.includes(inputValue)
+
+        return findData;
+      });
+
+      console.log(filteredNews);
+
+      if(filteredNews.length === 0){
+        waring.classList.replace("d-none" , "d-block");
+        rowNews.innerHTML=``
+        ulIndicators.classList.add("d-none")
+      }else{
+        ulIndicators.classList.replace("d-block" , "d-none")
+        waring.classList.replace("d-block" , "d-none");
+        showDataOnClickIndicator(filteredNews);
+      }
+    });
+  })
+  .catch((Error) => {
+    console.log("Error", Error);
+  });
+
+fetch(
+  "https://newsapi.org/v2/everything?q=apple&from=2025-10-01&to=2025-10-01&sortBy=popularity&apiKey=49878c3f53a943c28b30ba4476d1d610"
+)
+  .then((response) => response.json())
+  .then(function (data) {
     categoryApple = data.articles;
-}).catch((Error) => {
+  })
+  .catch((Error) => {
     console.log("Error", Error);
-})
+  });
 
-fetch("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=0a41a4f5b2174e4ea14efdebefdcbb6e").then((response) => response.json()).then(function (data) {
+fetch(
+  "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=49878c3f53a943c28b30ba4476d1d610"
+)
+  .then((response) => response.json())
+  .then(function (data) {
+    console.log(data);
     categorySource = data.articles;
-}).catch((Error) => {
+  })
+  .catch((Error) => {
     console.log("Error", Error);
-})
+  });
 
-fetch("https://newsapi.org/v2/everything?domains=wsj.com&apiKey=0a41a4f5b2174e4ea14efdebefdcbb6e").then((response) => response.json()).then(function (data) {
+fetch(
+  "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=49878c3f53a943c28b30ba4476d1d610"
+)
+  .then((response) => response.json())
+  .then(function (data) {
     categoryDomains = data.articles;
-}).catch((Error) => {
+  })
+  .catch((Error) => {
     console.log("Error", Error);
-})
+  });
 
-fetch("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0a41a4f5b2174e4ea14efdebefdcbb6e").then((response) => response.json()).then(function (data) {
+fetch(
+  "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=49878c3f53a943c28b30ba4476d1d610"
+)
+  .then((response) => response.json())
+  .then(function (data) {
     categoryCountry = data.articles;
-}).catch((Error) => {
+  })
+  .catch((Error) => {
     console.log("Error", Error);
-})
-
-
-
-
+  });
--- js/functions.js
+++ js/functions.js
@@ -1,73 +1,139 @@
-
-function showDataOfAPI(ArrayOfData, indexEndFun = 15, indexStart = 0, typeData = "tesla") {
-    let rowNews = document.querySelector("#Page .data .content-search .row");
-    ulIndicators = document.querySelector("#Page  .data .indicators ul");
-    indexEnd = indexEndFun;
-    rowNews.innerHTML += ``;
-    NumberAllObjInArray = ArrayOfData.length;
-
-    ulIndicators.innerHTML = `   
-    ${createIndicatorsContent(typeData)}
-    `;
-
-    ArrayOfData.forEach(function (data, numberOfCards) {
-        if (numberOfCards >= indexStart) {
-            if (numberOfCards < indexEndFun) {
-                rowNews.innerHTML += `
-                <div class="spacial-card mb-4 col-md-6 col-xl-4">
-                <div class="item" data-id="">
-                <div class="header"
-                style="background-image: url('${data.urlToImage}');">
-                </div>
-                <div class="body text-start">
-                <h6 class="NameUser">${data.source.id ?? data.source.name ?? "unknown"}</h6>
-                <h5 class="titleOfNew">
-                ${data.title.substr(0, 20)}...
-                </h5>
-                <p class="contentNew">
-                ${data.description.substr(0, 60) ?? "unknown"}
-                </p>
-                <span class="d-block mb-3 mainColor">
-                ${data.publishedAt} 
-                </span>
-                <a class="" href="${data.url}">See More</a>
-                </div>
-                </div>
-                </div>
-                `;
-            }
-            else {
-                return 0;
-            }
-        }
-    });
-
-    indexStart = indexEndFun;
-}
-
-function createIndicatorsContent(dataOfNews) {
+function createIndicatorsContent() {
     let HtmlContent = ``;
-    
-    for (let i = 0; i < Math.ceil(((NumberAllObjInArray / indexEnd))); i++) {
+    let numOfEnd = 15;
+
+    for (let i = 1; i <= Math.ceil(NumberAllObjInArray / numberCardsInRow); i++) {
         HtmlContent += ` 
-        <li value="${dataOfNews}" onclick="changeActives(this);typeOfNews(this , ${indexEnd * (i + 1)} , ${(indexEnd * (i + 1)) - indexEnd})" class="${(Math.ceil(((NumberAllObjInArray / indexEnd))) != i) ? "me-2" : ""} ${(i == 0) ? "active" : ""}">
-            ${i + 1}
+        <li value="${dataType}" onclick="changeActives(this);loopContent(${numOfEnd * i} , ${(i * numberCardsInRow) - numberCardsInRow} , this);"  class="${(Math.ceil(((NumberAllObjInArray / numberCardsInRow))) != i) ? "me-2" : ""} indicators ${(i == 1) ? "active clicked" : ""}">
+            ${i}
         </li> 
         `
     }
 
+    console.log(HtmlContent);
     return HtmlContent;
 }
-
+function updateIndicatorsContent() {
+    ulIndicators.innerHTML = ``;
+    ulIndicators.innerHTML = `   
+        ${createIndicatorsContent()}
+        `;
+    ulIndicators.classList.remove("d-none")
+}
 function changeActives(that) {
     let parentAllChildren = that.closest(".changeActives"),
         childActive = parentAllChildren.querySelector(".active"),
         currentChild = that;
 
-    childActive.classList.remove("active");
-    currentChild.classList.add("active");
+    if (that.classList.contains("active")) {
+        return 0
+    } else {
+        childActive.classList.remove("active");
+        currentChild.classList.add("active");
+    }
+
+    if (that.classList.contains("indicators")) {
+        childActive.classList.remove("clicked");
+        currentChild.classList.add("clicked");
+    }
 }
+function showDataOnClickIndicator(arrOfData) {
+    rowNews.innerHTML = ``;
 
+    arrOfData.forEach(function (data, index) {
+        itemIndex = index + 1
+        if (itemIndex <= numOfEnd && itemIndex >= numOfStart) {
+            rowNews.innerHTML += `
+                <div class="spacial-card mb-4 col-md-6 col-xl-4">
+                    <div class="item" data-id="${itemIndex}">
+                        <div class="header"
+                            style="background-image: url('${data.urlToImage}');">
+                        </div>
+                        <div class="body text-start">
+                            <h6 class="NameUser">${data.source.id ?? data.source.name ?? "unknown"}</h6>
+                            <h5 class="titleOfNew">
+                                ${data.title?.slice(0, 20) ?? "unknown"}...
+                            </h5>
+                            <p class="contentNew">
+                                ${data.description === "" ? "Nothing" : data.description?.slice(0, 50) ?? "unknown"}
+                            </p>
+                            <span class="d-block mb-3 mainColor">
+                                ${data.publishedAt} 
+                            </span>
+                            <a class="" href="${data.url}">See More</a>
+                        </div>
+                    </div>
+                </div>
+                `;
+            updateIndicatorsContent(dataType);
+        } else {
+            return 0;
+        }
+    })
+}
+function editDataOnClickIndicator(arrOfData) {
+    rowNews.innerHTML = ``;
 
+    arrOfData.forEach(function (data, index) {
+        itemIndex = index + 1
+        if (itemIndex <= numOfEnd && itemIndex > numOfStart) {
+            rowNews.innerHTML += `
+                <div class="spacial-card mb-4 col-md-6 col-xl-4">
+                    <div class="item" data-id="${itemIndex}">
+                        <div class="header"
+                            style="background-image: url('${data.urlToImage}');">
+                        </div>
+                        <div class="body text-start">
+                            <h6 class="NameUser">${data.source.id ?? data.source.name ?? "unknown"}</h6>
+                            <h5 class="titleOfNew">
+                                ${data.title?.slice(0, 20) ?? "unknown"}...
+                            </h5>
+                            <p class="contentNew">
+                                ${data.description === "" ? "Nothing" : data.description?.slice(0, 20)}
+                            </p>
+                            <span class="d-block mb-3 mainColor">
+                                ${data.publishedAt} 
+                            </span>
+                            <a class="" href="${data.url}">See More</a>
+                        </div>
+                    </div>
+                </div>
+                `;
 
+        } else {
+            return 0;
+        }
+    })
+}
+function typeOfNews(typeOfNews, go = false) {
+    if (typeOfNews == "tesla") {
+        ArrayOfData = categoryTesla;
+        dataType = typeOfNews;
+    } else if (typeOfNews == "apple") {
+        ArrayOfData = categoryApple;
+        dataType = typeOfNews;
+    } else if (typeOfNews == "sources") {
+        ArrayOfData = categorySource;
+        dataType = typeOfNews;
+    } else if (typeOfNews == "domains") {
+        ArrayOfData = categoryDomains;
+        dataType = typeOfNews;
+    } else if (typeOfNews == "country") {
+        ArrayOfData = categoryCountry;
+        dataType = typeOfNews;
+    } else {
+        console.log("I'm here");
+    }
 
+    NumberAllObjInArray = ArrayOfData.length;
+    if (go) {
+        editDataOnClickIndicator(ArrayOfData);
+    } else {
+        showDataOnClickIndicator(ArrayOfData);
+    }
+}
+function loopContent(endPoint, startPoint, that) {
+    numOfStart = (numberCardsInRow * that.textContent) - numberCardsInRow;
+    numOfEnd = endPoint;
+    typeOfNews(that.getAttribute("value"), true);
+}
